a789e11def3ed456b5bdaf0b538f5862047aaf9f
Philip M Carrillo <phil.carrillo@intl-data-eng.com>
2023-11-10 16:29:23 -0500

    Move bootstrap-icons to css assets/css

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -10,7 +10,7 @@ function u_enqueue(){
   
   wp_register_style(
     'u_bootstrap_icons',
-    get_theme_file_uri('assets/bootstrap-icons/bootstrap-icons.css')
+    get_theme_file_uri('assets/css/bootstrap-icons/bootstrap-icons.css')
   );
   wp_register_style(
     'u_theme',
