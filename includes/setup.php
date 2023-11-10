82281548867a8ad198ba070b597305eacf88a367
Philip M Carrillo <phil.carrillo@intl-data-eng.com>
2023-11-10 16:39:35 -0500

    Update paths for css files

--- includes/setup.php
+++ includes/setup.php
@@ -3,10 +3,10 @@ function u_setup_theme() {
   add_theme_support('editor-styles');
 
   add_editor_style([
-    'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
-    'assets/bootstrap-icons/bootstrap-icons.css',
-    'assets/public/index.css',
-    'assets/editor.css'
+    'https:/css.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
+    'assets/css/bootstrap-icons/bootstrap-icons.css',
+    'assets/css/index.css',
+    'assets/css/editor.css'
   ]);
 
 }
